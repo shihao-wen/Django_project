8d37fa4a7943678655eb62976692be35ccb43e5f
Shihao Wen <wenshihao22@gmail.com>
2018-12-11 16:38:27 -0500

    Migrate and add PostgreSQL database

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'Django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '1397890king',
+        'HOST':'localhost'
     }
 }
 
