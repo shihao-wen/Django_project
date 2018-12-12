1870236a7e78fd8bbef431c9b13123336f83d7e5
Shihao Wen <wenshihao22@gmail.com>
2018-12-12 01:32:20 -0500

    add context to view.py and use for loop in listings.html

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Listing
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context=context)
 
 
 def listing(request):
